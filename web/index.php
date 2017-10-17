1c9e181ff8656b8077f2c9893aa6d6aa3942f332
Nader Althubaity <na511dr@hotmail.com>
2017-10-17 00:31:00 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,22 @@
+
 <?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once 'Twilio/autoload.php'; // Loads the library
 
-require('../vendor/autoload.php');
+use Twilio\Rest\Client;
 
-echo "Nader is the best";
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = "ACc2d739e84d677db9f286438ec8441bc2";
+$token = "149c5535473312e2e75c2023d6fbe6cb";
+$client = new Client($sid,$token);
 
-?>
+$client->messages
+    ->create(
+        "+1",
+        array(
+            "from" => "+17732577791",
+            "body" => "This Message is being sent from Nader using Twilio!"
+        )
+    );
+echo "Massage has been sent successfully";
+
